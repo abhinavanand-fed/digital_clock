bd9021053d9396d1fb19542bf8b5a65a9c0be29b
abhinavanand-fed <abhinavanand080@gmail.com>
2023-01-31 20:40:43 +0530

    digital_clock.js.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./DigitalClock.css";
 
-function App() {
+function DigitalClock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="digital-clock">
+      <h1 className="digital-clock-time">{time}</h1>
     </div>
   );
 }
 
-export default App;
+export default DigitalClock;
